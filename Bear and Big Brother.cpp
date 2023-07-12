9696bc5a98e9ab06a207c82866a2f10d40b8d342
Anhcoder46 <tranduanh045@gmail.com>
2023-07-12 16:40:41 +0700

    code

--- Bear and Big Brother.cpp
+++ Bear and Big Brother.cpp
@@ -25,13 +25,13 @@ int main () {
 #endif
 	ios_base::sync_with_stdio(false);
 	ll a, b;
-	cin >> a >> b;
+	cin>>a>>b;
 	int res = 0;
 	while (a <= b) {
 		a *= 3;
 		b *= 2;
 		res++;
 	}
-	cout << res << endl;
+	cout<<res<<endl;
 	return 0;
 }
